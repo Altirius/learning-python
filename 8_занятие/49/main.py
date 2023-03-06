13d95059a44bb83f940f48b3fbb9bcd6430e376a
mv.baranov <mv.baranov@tensor.ru>
2023-03-07 00:06:44 +0300

    Занятие 8, Задание 49

--- 8_занятие/49/main.py
+++ 8_занятие/49/main.py
@@ -2,84 +2,124 @@ import json
 
 FILE_NAME = 'phones.json'
 
-def mainLoop():
-	contacts = loadData()
-	key = 0
+SHOW_LIST_KEY = 1
+SEARCH_CONTACT_KEY = 2
+ADD_CONTACT_KEY = 3
+DELETE_CONTACT_KEY = 4
+UPDATE_CONTACT_KEY = 5
+SAVE_LIST_KEY = 6
+QUIT_KEY = 7
 
-	while key != 5:
-		printMainMenu()
-		key = inputActionKey()
+class ContactsDirectory(object):
+	def __init__(self, fileName):
+		self.fileName = fileName
 
-		if key == 1:
-			printContacts(contacts)
-		elif key == 2:
-			handleSearch(contacts)
-		elif key == 3:
-			contacts.append(inputContact())
+	def mainLoop(self):
+		self.contacts = self.loadData()
+		key = 0
 
-def loadData():
-	with open(FILE_NAME, 'r', encoding='utf-8') as f:
-		return json.load(f)
+		while key != QUIT_KEY:
+			self.printMainMenu()
+			key = self.inputActionKey()
+
+			if key == SHOW_LIST_KEY:
+				self.printContacts(self.contacts)
+			elif key == SEARCH_CONTACT_KEY:
+				self.handleSearch()
+			elif key == ADD_CONTACT_KEY:
+				self.handleAddContact()
+			elif key == DELETE_CONTACT_KEY:
+				self.handleDelete()
+			elif key == UPDATE_CONTACT_KEY:
+				self.handleUpdate()
+			elif key == SAVE_LIST_KEY:
+				self.writeData()
+
+	def loadData(self):
+		with open(self.fileName, 'r', encoding='utf-8') as f:
+			return json.load(f)
 	
-	
-def printMainMenu():
-	print('Вывести весь список (1) | Поиск по строке (2) | Добавить контакт (3) | Удалить контакт (4) | Выйти (5)')
+	def writeData(self):
+		with open(self.fileName, 'w', encoding='utf-8') as f:
+			f.write(json.dumps(self.contacts, ensure_ascii=False))
 
-def inputActionKey():
-	return int(input('Введите числовой идентификатор желаемого действия: '))
-
-def writeData(data):
-	with open(FILE_NAME, 'w', encoding='utf-8') as f:
-		f.write(json.dumps(data, ensure_ascii=False))
-
-def printContacts(contacts):
-	if isContactsEmpty(contacts):
-		printEmptyContactsMessage()
-
-	for contact in contacts:
-		printContact(contact)
-
-def isContactsEmpty(contacts):
-	return len(contacts) == 0
-
-def printEmptyContactsMessage():
-	print('Записей не найдено')
-
-def printContact(contact):
-	print(
-		'Имя: ', contact.get('firstName'), ' | ',
-		'Фамилия: ', contact.get('lastName'), ' | ',
-		'Номер телефона: ', contact.get('phoneNumber')
-	)
-
-def handleSearch(contacts):
-	return printContacts(filterContactsBySearchString(contacts, inputSearchString()))
+	def printMainMenu(self):
 		
-def inputSearchString():
-	return input('Введите строку для поиска: ')
+		print(
+			'Вывести весь список (' + str(SHOW_LIST_KEY) + ') |',
+			'Поиск (' + str(SEARCH_CONTACT_KEY) + ') |',
+			'Добавить (' + str(ADD_CONTACT_KEY) + ') |',
+			'Удалить (' + str(DELETE_CONTACT_KEY) + ') |',
+			'Обновить (' + str(UPDATE_CONTACT_KEY) + ') |',
+			'Сохранить(' + str(SAVE_LIST_KEY) + ') |',
+			'Выйти(' + str(QUIT_KEY) + ')'
+		)
 
-def filterContactsBySearchString(contacts, searchString):
-	result = []
-	for contact in contacts:
-		if isSearchStringInContact(searchString, contact):
-			result.append(contact)
-	return result
+	def inputActionKey(self):
+		return int(input('Введите числовой идентификатор желаемого действия: '))
 
-def isSearchStringInContact(searchString, contact):
-	return (
-		searchString in contact.get('firstName') or
-		searchString in contact.get('lastName') or
-		searchString in contact.get('phoneNumber')
-	)
+	def printContacts(self, contacts):
+		if self.isContactsEmpty():
+			self.printEmptyContactsMessage()
 
-def inputContact():
-	return {
-		"firstName": input('Введите имя: '),
-		"lastName": input('Введите фамилию: '),
-		"phoneNumber": input('Введите номер телефона: ')
-	}
+		for index, contact in enumerate(contacts):
+			self.printIndexedContact(index, contact)
 
+	def isContactsEmpty(self):
+		return len(self.contacts) == 0
+
+	def printEmptyContactsMessage(self):
+		print('Записей не найдено')
+
+	def printIndexedContact(self, index, contact):
+		print(
+			index, ' | '
+			'Имя: ', contact.get('firstName'), ' | ',
+			'Фамилия: ', contact.get('lastName'), ' | ',
+			'Номер телефона: ', contact.get('phoneNumber')
+		)
+
+	def handleSearch(self):
+		return self.printContacts(self.filterContactsBySearchString(self.inputSearchString()))
+			
+	def inputSearchString(self):
+		return input('Введите строку для поиска: ')
+
+	def filterContactsBySearchString(self, searchString):
+		result = []
+		for contact in self.contacts:
+			if self.isSearchStringInContact(searchString, contact):
+				result.append(contact)
+		return result
+
+	def isSearchStringInContact(self, searchString, contact):
+		return (
+			searchString in contact.get('firstName') or
+			searchString in contact.get('lastName') or
+			searchString in contact.get('phoneNumber')
+		)
+
+	def handleAddContact(self):
+		self.contacts.append({
+			"firstName": input('Введите имя: '),
+			"lastName": input('Введите фамилию: '),
+			"phoneNumber": input('Введите номер телефона: ')
+		})
+
+	def handleDelete(self):
+		position = int(input('Введите порядковый номер записи которую хотите удалить: '))
+		self.printIndexedContact(position, self.contacts[position])
+		self.contacts.pop(position)
 	
+	def handleUpdate(self):
+		position = int(input('Введите порядковый номер записи которую хотите обновить: '))
+		self.printIndexedContact(position, self.contacts[position])
+		self.contacts[position] = ({
+			"firstName": input('Введите имя: '),
+			"lastName": input('Введите фамилию: '),
+			"phoneNumber": input('Введите номер телефона: ')
+		})
 
 
-mainLoop()
+dictionary = ContactsDirectory(FILE_NAME)
+dictionary.mainLoop()
